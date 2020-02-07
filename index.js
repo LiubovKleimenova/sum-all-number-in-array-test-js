fc955aeb6a4d71ffcb45dc65baf4a1318f5cf462
LiubovKleimenova <kleimenovaliubov@gmail.com>
2020-02-07 13:38:51 -0500

    added fn to sum nesting arrays

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 function sumItems(array) {
+  let sum = 0;
   // Sum all the numbers in the array
+  array.forEach(element => {
+    if (Array.isArray(element)) {
+      sum += sumItems(element);
+    } else {
+      sum += element;
+    }
+  });
+  return sum;
 }
 
 module.exports = sumItems;
